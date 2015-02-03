3f89e54bcc066d1f9b3094af9bce97e1d7fdbc3e
Andy Long [work] <andy@theouteredge.co.uk>
2015-02-03 21:33:22 +0000

    fixing saving account interest rate

---
+++ MidlandsBank.Domain/SavingsAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidlandsBank.Domain
+{
+    public class SavingsAccount : Account, IAquireInterest
+    {
+        public SavingsAccount()
+        {
+            // for serialisation
+            InterestRate = 3;
+        }
+
+        public SavingsAccount(int accountNumber, string accountHoldersName, double openingDeposit)
+            : base(accountNumber, accountHoldersName, openingDeposit)
+        {
+        }
+
+        public override void Withdraw(double amount, string description)
+        {
+            if (CurrentBalance() - amount < 0)
+                throw new ArgumentException("Insufficient funds available for this transaction");
+            
+            base.Withdraw(amount, description);
+        }
+
+        public double InterestRate { get; set; }
+        public void CalculateAndApplyInterest()
+        {
+            double interestAquired = CurrentBalance() * (InterestRate/100);
+            Transactions.Add(new Transaction(interestAquired, CurrentBalance(), "Monthly Interest Rate"));
+        }
+    }
+}
