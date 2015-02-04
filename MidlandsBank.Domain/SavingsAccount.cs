a56ba2f29bc816a219df1f3aa6d5a6d99c4daaf4
Andy Long [work] <andy@theouteredge.co.uk>
2015-02-04 10:11:20 +0000

    Recreated the SavingsAccount class

---
+++ MidlandsBank.Domain/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MidlandsBank.Domain
+{
+    public class SavingsAccount : Account
+    {
+        public SavingsAccount()
+        {
+            // for serialisation :/
+        }
+
+        public SavingsAccount(int accountNumber, string accountHoldersName, double openingDeposit)
+            : base(accountNumber, accountHoldersName, openingDeposit)
+        {
+            
+        }
+
+        public override void Withdraw(double amount, string description)
+        {
+            if (CurrentBalance() - amount < 0)
+                throw new ArgumentException("The account has insufficient funds to complete the Transactions", "amount");
+            
+            base.Withdraw(amount, description);
+        }
+    }
+}
